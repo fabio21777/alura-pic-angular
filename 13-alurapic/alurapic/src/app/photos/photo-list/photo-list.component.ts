272b0ed7e5377fa37f5c11e0de2fd4111a9da4ac
fabio <fabio@itexto.com.br>
2022-11-14 08:31:52 -0300

    forçando o componente a recarregar quando o usuario mudar a rota

--- 13-alurapic/alurapic/src/app/photos/photo-list/photo-list.component.ts
+++ 13-alurapic/alurapic/src/app/photos/photo-list/photo-list.component.ts
@@ -1,39 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 
-import { Photo } from '../photo/photo';
-import { PhotoService } from '../photo/photo.service';
+import { Photo } from "../photo/photo";
+import { PhotoService } from "../photo/photo.service";
 
 @Component({
-  selector: 'app-photo-list',
-  templateUrl: './photo-list.component.html',
-  styleUrls: ['./photo-list.component.css']
+  selector: "app-photo-list",
+  templateUrl: "./photo-list.component.html",
+  styleUrls: ["./photo-list.component.css"],
 })
 export class PhotoListComponent implements OnInit {
-
   photos: Photo[] = [];
-  filter: string = '';
+  filter: string = "";
   hasMore: boolean = true;
   currentPage: number = 1;
-  userName: string = '';
+  userName: string = "";
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private photoService: PhotoService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
-    this.userName = this.activatedRoute.snapshot.params.userName;
-    this.photos = this.activatedRoute.snapshot.data['photos'];
+    this.activatedRoute.params.subscribe((params) => {
+      this.userName = params.userName;
+      this.photos = this.activatedRoute.snapshot.data["photos"];
+    });
   }
 
   load() {
     this.photoService
       .listFromUserPaginated(this.userName, ++this.currentPage)
-      .subscribe(photos => {
-        this.filter = '';
+      .subscribe((photos) => {
+        this.filter = "";
         this.photos = this.photos.concat(photos);
-        if(!photos.length) this.hasMore = false;
+        if (!photos.length) this.hasMore = false;
       });
   }
 }
