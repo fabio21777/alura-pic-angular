189bde1325c03d802f2808cd596909e094734cf2
fabio21777 <fabiosouza21777@gmail.com>
2022-11-27 17:35:12 -0300

    deretiva show if

--- 13-alurapic/alurapic/src/app/photos/photo-details/photo-details.module.ts
+++ 13-alurapic/alurapic/src/app/photos/photo-details/photo-details.module.ts
@@ -1,3 +1,4 @@
+import { ShowIfLoggedModule } from './../../shared/directives/show-if-logged/show-if-logged.module';
 import { PhotoOwnerOnlyDirective } from './photo-owner-only/photo-owner-only.directive';
 import { NgModule } from "@angular/core";
 import { PhotoDetailsComponent } from "./photo-details.component";
@@ -17,7 +18,8 @@ import { VMessageModule } from 'src/app/shared/components/vmessage/vmessage.modu
         PhotoModule,
         RouterModule,
         ReactiveFormsModule,
-        VMessageModule
+        VMessageModule,
+        ShowIfLoggedModule
     ]
 })
 export class PhotoDetailsModule { }
